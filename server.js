8a17d45ec88e5b5e1a2edde04b2638f3eff68c5b
maniacinahuel <89110865+ManiaciNahuel@users.noreply.github.com>
2022-06-29 22:31:34 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -38,8 +38,10 @@ app.get('/productoRandom', (request, respuesta) => {
     respuesta.send(productos[indice])
 })
 
-const server = app.listen(8080, () => {
-    console.log("Server is running on port 8080");
+const PORT = process.env.PORT || 8080;
+
+const server = app.listen(PORT, () => {
+    console.log("Servidor HTTP escuchando en el puerto " + server.address().port);
 })
 
 server.on("error", error => console.log(error))
